4a9bc9b0a3f48c53a3acbfad07ebb3ebec76c64d
arthursmiles <arthurmadala@gmail.com>
2023-04-24 12:20:03 -0700

    ext_line.c

---
+++ ext_line.c
@@ -0,0 +1,73 @@
+#include "main.h"
+/**
+ * returns the length of a string
+ *
+ * returns length
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	if (!s)
+		return (0);
+
+	while (*s++)
+		i++;
+	return (i);
+}
+
+/**
+ * make comparison of two strings
+ * 1st and
+ * 2nd
+ * 
+ * returns negative, positive or zero
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 && *s2)
+	{
+		if (*s1 != *s2)
+			return (*s1 - *s2);
+		s1++;
+		s2++;
+	}
+	if (*s1 == *s2)
+		return (0);
+	else
+		return (*s1 < *s2 ? -1 : 1);
+}
+
+/**
+ * checks code
+ * 
+ * returnd adress of char
+ */
+char *starts_with(const char *haystack, const char *needle)
+{
+	while (*needle)
+		if (*needle++ != *haystack++)
+			return (NULL);
+	return ((char *)haystack);
+}
+
+/**
+ * _strcat - concatenates two strings.
+ * @dest: the destination buffer.
+ * @src: the source buffer
+ *
+ * Return: pointer to destination buffer
+ */
+char *_strcat(char *dest, char *src)
+{
+	char *ret = dest;
+
+	while (*dest)
+		dest++;
+	while (*src)
+		*dest++ = *src++;
+	*dest = *src;
+	return (ret);
+}
+
